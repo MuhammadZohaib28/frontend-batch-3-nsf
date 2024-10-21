24d3c743d030f184a170e90c11e61072f0183d62
Muhammad Zohaib <muhammad.zohaib@miletap.com>
2024-10-21 22:10:44 +0500

    AboutMe Section

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -1,10 +1,80 @@
 import React from 'react'
 import './AboutMe.css'
+import profilepicture from '../../images/pp.avif'
 
 const AboutMe = () => {
   return (
-    <div>
+    <div className='aboutme'>
+
       <h1>About Me</h1>
+
+      <div className='aboutme-midsection'>
+
+        <div className='mainsection-top-profile-picture   aboutme-midsection-picture'>
+          <img src={profilepicture} />
+        </div>
+
+
+        <div className='aboutme-cardsection'>
+          <div>
+            <p className='p'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptates laborum nesciunt mollitia rem nost rum accusantium, iste quo a laboriosam nulla nobis repellendus ullam quibusdam fugit, praesentium sequi iusto. Impedit.</p>
+
+            <button>Download CV</button>
+          </div>
+
+
+
+
+
+          <div className='aboutme-bars-section'>
+            <div className='aaa'>
+              <h3>Development</h3>
+              <span>90%</span>
+            </div>
+
+            <span  className='aboutme-cardsection-bars'></span>
+
+
+
+
+
+            <div className='aaa'>
+              <h3>UI/UX design</h3>
+              <span>80%</span>
+
+
+            </div>
+
+            <span className='aboutme-cardsection-bars2'></span>
+
+
+            <div className='aaa'>
+              <h3>Photography</h3>
+              <span>60%</span>
+
+
+            </div>
+
+            <span className='aboutme-cardsection-bars3'></span>
+
+
+
+          </div>
+        </div>
+
+      </div>
+
+
+
+
+
+
+
+      <div >
+
+      </div>
+
+
     </div>
   )
 }
--- src/components/mainSection/MainSection.jsx
+++ src/components/mainSection/MainSection.jsx
@@ -1,12 +1,80 @@
 import React from 'react'
 import './MainSection.css'
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import profilepicture from '../../images/pp.avif'
+
+
+
 
 const MainSection = () => {
+
+    const mainSectionIcons = [
+        {
+            icon: <FaInstagram className='mainsection-singleIcon'/>,
+            link: 'https://www.instagram.com/'
+        },
+        {
+            icon: <FaLinkedin />,
+            link: 'https://www.linkedin.com/'
+        },
+        {
+            icon: <FaGithub />,
+            link: 'www.github.com'
+        },
+        {
+            icon: <FaTwitter />,
+            link: 'https://twitter.com/'
+        },
+        {
+            icon: <FaFacebook />,
+            link: 'https://www.facebook.com/'
+        },
+        {
+            icon: <FaYoutube />,
+            link: 'https://www.youtube.com/'
+        }
+    ]
+
     return (
         <div id='#Home' className='mainsection'>
-            <h1>Main Section</h1>
+            <div className='mainsection-top'>
+                <div className='mainsection-top-profile-picture'>
+                    <img src={profilepicture} alt='Profile Picture' />
+                </div>
+
+                <h1>Farhan Zafar</h1>
+
+
+                <p>I'm Front-End Developer</p>
+
+
+                <ul className='mainsection-icons'>
+                    {mainSectionIcons.map((item, index) => {
+                        return (
+                            <a key={index} href={item.link} target='_blank'>
+                                <li className='mainsection-singleIcon'>{item.icon}</li>
+                            </a>
+                        )
+                    })}
+                </ul>
+
+                <button className='mainsection-button'>Download CV</button>
+
+
+            </div>
+
+            {/* <div className='mainsection-bottom'>
+                <h1>Bottom Section</h1>
+            </div> */}
         </div>
     )
 }
 
 export default MainSection
+
+
