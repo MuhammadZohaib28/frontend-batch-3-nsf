8e012ca588fd0a4eb3292f4af62e1c23ce961e07
Muhammad Zohaib <muhammad.zohaib@miletap.com>
2024-10-22 22:13:32 +0500

    Services Section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
 import './Services.css'
+import cardpicture1 from '../../images/cardpic.jpg'
 
 const Services = () => {
+  const servicesCards = [
+    {
+      image: cardpicture1,
+      title: "UI/UX Design",
+      description: "I can design beautiful user interfaces and user experiences for your website or mobile application.",
+      bgColor: '#FFE700'
+
+    },
+    {
+      image: "dummy image",
+      title: "Web Development",
+      description: "I can design beautiful user interfaces and user experiences for your website or mobile application.",
+      bgColor: "#0D92F4"
+    },
+    {
+      image: "dummy image",
+      title: "Photography",
+      description: "I can design beautiful user interfaces and user experiences for your website or mobile application.",
+      bgColor: "#FF4D4D"
+    }
+  ]
   return (
-    <div>
+    <div className='services'>
       <h1>Services</h1>
+
+
+
+      <div className='services-cardsection'>
+
+        {servicesCards.map((card, index) => {
+          return (
+            <div style={{ backgroundColor: card.bgColor }} key={index} className='services-singlecard'>
+              <div className='image-wrapper'>
+                <img src={card.image} alt={card.title}
+                  className='image' />
+
+              </div>
+
+              <h1>{card.title}</h1>
+
+              <p>{card.description}</p>
+
+            </div>
+          )
+        })}
+
+
+
+      </div>
     </div>
   )
 }
